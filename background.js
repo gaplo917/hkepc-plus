703674ae823b270c66e206019b12f80c51cc8fb3
Gary Lo <logap@me.com>
2015-05-03 15:46:56 +0800

    bug caused by the comment in theme.json
    improve performance

--- background.js
+++ background.js
@@ -3,48 +3,56 @@
  */
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	var pattern = new RegExp("^(http://www.hkepc.com/forum)");
+    // Only fire the when loading ( to prevent it fire in status === complete)
+    if (changeInfo.status === "loading"){
 
-	if(pattern.test(tab.url)) {
+        var pattern = new RegExp("^(http://www.hkepc.com/forum)");
 
-		chrome.storage.sync.get('theme', function (items) {
+        if(pattern.test(tab.url)) {
 
-            // ONLY if the theme is set
-			if (items.theme !== undefined) {
-                getJSON(chrome.runtime.getURL(items.theme + '/theme.json'),function(response){
-                    _.each(response.css, function (css) {
-                        chrome.tabs.insertCSS(tabId, {
-                            file: items.theme + '/' + css,
-                            allFrames: false,
-                            runAt: "document_start"
+            chrome.storage.sync.get('theme', function (items) {
+
+                // ONLY if the theme is set
+                if (items.theme !== undefined) {
+                    getJSON(chrome.runtime.getURL(items.theme + '/theme.json'),function(response){
+                        _.each(response.css, function (css) {
+                            chrome.tabs.insertCSS(tabId, {
+                                file: items.theme + '/' + css,
+                                allFrames: false,
+                                runAt: "document_start"
+                            });
                         });
+
+                        // For each js
+                        _.each(response.js, function (js) {
+                            chrome.tabs.executeScript(tabId, {
+                                file:items.theme +'/' + js,
+                                runAt: "document_start"
+                            });
+                        });
+
                     });
 
-                    // For each js
-                    _.each(response.js, function (js) {
-                        chrome.tabs.executeScript(tabId, {
-                            file:items.theme +'/' + js,
-                            runAt: "document_start"
-                        });
-                    });
+                }
 
-                });
-
-			}
-
-		});
-	}
+            });
+        }
+    }
 
 });
 
 function getJSON(url,cb){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url , true);
-    xhr.setRequestHeader("Content-Type","text/html");
+    xhr.setRequestHeader("Content-Type","application/json");
     xhr.send();
     xhr.onreadystatechange = function()
     {
-        cb(JSON.parse(xhr.response));
+        try{
+            cb(JSON.parse(xhr.response));
+        }catch(err){
+            //Known error  (Uncaught SyntaxError: Unexpected end of input) on v8 compiler, no need to handle
+        }
     }
 }
 
