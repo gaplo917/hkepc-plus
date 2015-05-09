4b4721b0468e198081691e797d80c4e1df2b20f6
Gary Lo <logap@me.com>
2015-05-09 23:23:53 +0800

    added warm theme

---
+++ content/theme/warm/UIhacks/threadColorInherit.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Gaplo917 on 3/5/15.
+ */
+$(function(){
+
+    $('.datatable tr .subject a').each(function () {
+        var inlineStyle = $(this).attr('style');
+        if(inlineStyle !== undefined){
+            var splited = inlineStyle.match(/#(?:[0-9a-f]{3}){1,2}$/i);
+            if(splited == null) return;
+            var color = splited[0],
+                rgb = hexToRgb(color.trim()),
+                rgbaArr = [];
+            rgbaArr.push(Math.abs(rgb.r));
+            rgbaArr.push(Math.abs(rgb.g));
+            rgbaArr.push(Math.abs(rgb.b));
+            rgbaArr.push(1);
+
+            $(this).attr('style','color:rgba('+ rgbaArr.join(',')+')!important');
+        }
+    });
+});
