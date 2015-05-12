1553930dab5b5369a935d8ee4d1653adeba9cfc7
loyuehei <logap@me.com>
2015-05-12 16:39:03 +0800

    update action bar icon

---
+++ content/browser_action/popup.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Gaplo917 on 12/5/15.
+ */
+$(function () {
+    $('#github').attr('src','http://ghbtns.com/github-btn.html?user=gaplo917&repo=hkepc-plus&type=watch&count=true');
+});
