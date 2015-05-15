af43c5ea84c08754c0a43750811e33626b90b130
loyuehei <logap@me.com>
2015-05-15 17:10:32 +0800

    UI bug fixed on search page

--- content/addons/betterButton/js/betterButton.js
+++ content/addons/betterButton/js/betterButton.js
@@ -4,7 +4,7 @@
 $(function(){
 
     // Change the button UI
-    $('.pmreply button, .btnbar button').each(function () {
+    $('.pmreply button, .btnbar button,#searchsubmit').each(function () {
         $(this).addClass('btn btn-info postbtn');
     });
 
